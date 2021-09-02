6799fcfe9bced13c857d4a9a54a5edaa38f43641
Samuel-prog182 <samuellb.o.w.182@hotmail.com>
2021-09-02 17:42:03 +0000

    long no lugar do int

--- Semana5_exer1.java
+++ Semana5_exer1.java
@@ -2,6 +2,7 @@ public class Semana5_exer1{
     public static void main(String args []){
         //fazer o fatorial , testar  20, 30..40;
         //5*4*3*2*1 = 120
+        // long no lugar de int para numeros maiores
         long fatorial=1;
         for(int f=5;f>=1;f--){
             fatorial=f*fatorial;
